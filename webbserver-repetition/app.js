62bb16d672804bf93bf32449d53fe7fe7e9ba140
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-01-27 16:21:55 +0100

    Repetetion av server

--- webbserver-repetition/app.js
+++ webbserver-repetition/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
+
+const namnListaSchema = new mongoose.Schema({
+  names: Array,
+});
+
+const NamnLista = mongoose.model("NamnLista", namnListaSchema);
 
 app.use(express.static(__dirname + "\\client"));
 
@@ -13,6 +30,17 @@ const namn = [];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { names: namn });
+});
+
+app.get("/Save", (req, res) => {
+    var namnList = NamnLista({
+      names: namn,
+    });
+
+  namnList.save();
+
+  namn.splice(0, namn.length);
+  res.redirect("/");
 });
 
 app.post("/", function (req, res) {
